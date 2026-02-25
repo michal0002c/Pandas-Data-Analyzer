eff728418bc2a2b6cc4fcc66c23f7b61a43a23e0
michal0002c <michal.kasperek0002c@gmail.com>
2026-02-25 22:23:31 +0100

    Add GUI and file loading

--- main.py
+++ main.py
@@ -34,6 +34,22 @@ def load_file():
 btn_load = tk.Button(app, text="Load file", command=load_file)
 btn_load.pack()
 
+text = tk.Text(app, height=15)
+text.pack()
+
+def show_data():
+    global df
+
+    if df is None:
+        messagebox.showerror("Error", "No data")
+        return
+    
+    text.delete(1.0, tk.END)
+    text.insert(tk.END, df.head().to_string())
+
+btn_show = tk.Button(app, text="Show Data", command=show_data)
+btn_show.pack()
+
 app.mainloop()
 
 
